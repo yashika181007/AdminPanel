5145bff518203d36f039b9f9c3e6a15e4f29db7e
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-08-14 16:17:34 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'megastores',
+    user: 'u510451310_megastore',
+    password: 'U510451310_megastore',
+    database: 'u510451310_megastore',
     waitForConnections: true,
     // connectionLimit: 10,
     // queueLimit: 0
