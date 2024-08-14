630507b39622e94fd718e51d7654270321b19e29
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-08-14 16:22:58 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 
+// Create a connection pool
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'u510451310_megastore',
@@ -9,4 +10,17 @@ const pool = mysql.createPool({
     // connectionLimit: 10,
     // queueLimit: 0
 });
+
+// Attempt to establish a connection
+pool.getConnection((err, connection) => {
+    if (err) {
+        // If there's an error, display it
+        console.error('Error connecting to the database:', err.message);
+    } else {
+        // If successful, display a connected message
+        console.log('Connected to the database!');
+        connection.release(); // Release the connection back to the pool
+    }
+});
+
 module.exports = pool.promise();
